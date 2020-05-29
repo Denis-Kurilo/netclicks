b9dc18cbf5a002f99033d1dcb8be120d477369dc
Kurilo Denis <webprogram2017@mail.ru>
2020-05-29 17:29:55 +0300

    added click menu

--- js/main.js
+++ js/main.js
@@ -16,8 +16,12 @@ const leftMenu = document.querySelector('.left-menu'),
   description = document.querySelector('.description'),
   modalLink = document.querySelector('.modal__link'),
   searchForm = document.querySelector('.search__form'),
-  searchFormInput = document.querySelector('.search__form-input');
-
+  searchFormInput = document.querySelector('.search__form-input'),
+  preloader = document.querySelector('.preloader'),
+  dropdown = document.querySelectorAll('.dropdown'),
+  tvShowsHead = document.querySelector('.tv-shows__head'),
+  posterWrapper = document.querySelector('.poster__wrapper'),
+  modalContent = document.querySelector('.modal__content');
 
 const loading = document.createElement('div');
 
@@ -26,6 +30,7 @@ console.log(loading)
 
 const DBService = class {
   getData = async (url) => {
+  	tvShows.append(loading);
     const res = await fetch(url);
 
     if(res.ok){
@@ -48,22 +53,42 @@ const DBService = class {
   }
 
   getTvShow = (id) => {
-    return this.getData(`${this.SERVER}/tv/${id}?api_key=${this.API_KEY}&language=ru-RU`);
-    
+    return this.getData(`${SERVER}/tv/${id}?api_key=${API_KEY}&language=ru-RU`);
   }
+
+  getTopRated = () => this.getData(`${SERVER}/tv/top_rated?api_key=${API_KEY}&language=ru-RU`);
+
+  getPopular = () => this.getData(`${SERVER}/tv/popular?api_key=${API_KEY}&language=ru-RU`);
+
+  getToday = () => this.getData(`${SERVER}/tv/airing_today?api_key=${API_KEY}&language=ru-RU`);
+
+  getWeek = () => this.getData(`${SERVER}/tv/on_the_air?api_key=${API_KEY}&language=ru-RU`);
 }
 
+const dbService = new DBService();
 
-const renderCard = response => {
+
+const renderCard = (response, target) => {
   console.log(response);
   tvShowsList.textContent = '';
+
+  if(!response.total_results){
+  	loading.remove();
+  	tvShowsHead.textContent = 'По вашему запросу ничего не найдено.';
+  	tvShowsHead.style.cssText = 'color: red; font-size: 20px';
+  	return;
+  }
+
+  tvShowsHead.textContent = target ? target.textContent : 'Результат поиска';
+  tvShowsHead.style.cssText = '';
 
   response.results.forEach(item => {
     const { 
         backdrop_path: backdrop, 
         name: title, 
         poster_path: poster, 
-        vote_average: vote 
+        vote_average: vote,
+        id 
       } = item;
 
       console.log(typeof('vote'))
@@ -73,9 +98,10 @@ const renderCard = response => {
     const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '' ;
 
     const card = document.createElement('li');
+    card.idTV = id;
     card.className = 'tv-shows__item';
     card.innerHTML = `
-      <a href="#" class="tv-card">
+      <a href="#" id="${id}" class="tv-card">
           ${voteElem}
           <img class="tv-card__img"
                src="${posterIMG}"
@@ -94,27 +120,34 @@ searchForm.addEventListener('submit', (event) => {
   searchFormInput.value = '';
   
   if(value){
-    tvShows.append(loading);
-    new DBService().getSearchResult(value).then(renderCard);
+    dbService.getSearchResult(value).then(renderCard);
   }
 })
 
 
 //открытие - закрытие меню
+const closeDropDown = () => {
+	dropdown.forEach( (item) => {
+		item.classList.remove('active');
+	})
+};
+
 hamburger.addEventListener('click',() => { 
   leftMenu.classList.toggle('openMenu');
   hamburger.classList.toggle('open');
+  closeDropDown();
 });
 
 document.body.addEventListener('click', (event) => {
   if(!event.target.closest('.left-menu')){
     leftMenu.classList.remove('openMenu');
     hamburger.classList.remove('open');
-
+    closeDropDown();
   }
 });
 
 leftMenu.addEventListener('click', (event) => {
+	event.preventDefault();
   const target = event.target;
   const dropdown = target.closest('.dropdown');
 
@@ -122,6 +155,27 @@ leftMenu.addEventListener('click', (event) => {
     dropdown.classList.toggle('active');
     leftMenu.classList.add('openMenu');
     hamburger.classList.add('open');
+  }
+
+  if(target.closest('#top-rated')){
+  	dbService.getTopRated().then((response) => renderCard(response, target));
+  }
+
+  if(target.closest('#popular')){
+  	dbService.getPopular().then((response) => renderCard(response, target));
+  }
+
+  if(target.closest('#today')){
+  	dbService.getToday().then((response) => renderCard(response, target));
+  }
+
+  if(target.closest('#week')){
+  	dbService.getWeek().then((response) => renderCard(response, target));
+  }
+
+  if(target.closest('#search')){
+  	tvShowsList.textContent = '';
+  	tvShowsHead.textContent = '';
   }
 });
 
@@ -145,29 +199,48 @@ tvShowsList.addEventListener('click', (event) => {
   const target = event.target;
   const card = target.closest('.tv-card');
   if(card){
-    new DBService().getTestCard()
-        .then(response => {
-          tvCardImg.src = IMG_URL + response.poster_path;
-          modalTitle.textContent = response.name;
+  	preloader.style.display="block";
+    dbService.getTvShow(card.id)
+        .then(({ 
+        	poster_path: posterPath, 
+        	name: title, 
+        	genres, 
+        	vote_average: voteAverage, 
+        	overview, 
+        	homepage }) => {
+
+        	if(posterPath){
+        		tvCardImg.src = IMG_URL + posterPath;
+        		tvCardImg.alt = title;
+        		posterWrapper.style.display = '';
+        		modalContent.style.paddingLeft = '';
+        	}else{
+        		posterWrapper.style.display = 'none';
+        		modalContent.style.paddingLeft = '25px';
+        	}
+          modalTitle.textContent = title;
           /*genresList.innerHTML = response.genres.reduce((acc, item) => {
             return `${acc}<li>${item.name}</li>`
           }, '');*/
           genresList.textContent = '';
-          for (const item of response.genres){
-            genresList.innerHTML += `<li>${item.name}</li>`;
+          for (const item of genres){
+            genresList.innerHTML += '<li>' + item.name + '</li>';
           }
-          rating
-          description
-          modalLink
+          rating.textContent = voteAverage;
+          description.textContent = overview;
+          modalLink.href = homepage;
         })
-        .then(() => {
-          document.body.style.overflow = 'hidden';
-          modal.classList.remove('hide'); 
-        })
-  }
+      .then(() => {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide'); 
+      })
+      .finally(() => {
+      	preloader.style.display="none";
+      });
+		}
 });
 
-//закрытие модалки
+/*закрытие модалки*/
 modal.addEventListener('click', (event) => {
   if(event.target.closest('.cross') || event.target.classList.contains('modal')){
     document.body.style.overflow = '';
